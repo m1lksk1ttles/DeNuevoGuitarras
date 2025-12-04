85babc886ebf71fe34f7f11dad44506ef99be390
Juan Jaime Gutierrez <m1lksk1ttles@outlook.com>
2025-12-04 08:45:27 -0600

    kuke es un imbecil

--- back/database.py
+++ back/database.py
@@ -8,6 +8,8 @@ load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 print(f"URL de Base de Datos: {SQLALCHEMY_DATABASE_URL}")
+if SQLALCHEMY_DATABASE_URL and SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
