2adbb28d663a6cbb069f97273c7047b25e691106
Juan Jaime Gutierrez <m1lksk1ttles@outlook.com>
2025-12-07 14:20:31 -0600

    railway

--- back/database.py
+++ back/database.py
@@ -7,9 +7,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
-print(f"URL de Base de Datos: {SQLALCHEMY_DATABASE_URL}")
-if SQLALCHEMY_DATABASE_URL and SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
-    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# Debug (puedes quitar este print después)
+print("URL final:", SQLALCHEMY_DATABASE_URL)
+
+# Crear engine
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
